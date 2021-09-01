d2c24367513fb6f2f6317589aa851ff272bd1e1c
gianluca <gianluca_1227@hotmail.com>
2021-09-01 09:12:39 -0400

    Created test file for Pawn class
    implemented direction method and tests for method

--- lib/pieces/pawn.rb
+++ lib/pieces/pawn.rb
@@ -2,6 +2,14 @@ require_relative "./abstract_piece"
 
 class Pawn < AbstractPiece
 
-  
+  def create_moves(rank_index, file_index)
+    moves = []
+    moves << AbstractMove.new("")
+    return moves.compact
+  end
 
+  def direction
+    d = color == "white" ? -1 : 1
+    return d
+  end
 end
---
+++ spec/pawn_spec.rb
@@ -0,0 +1,42 @@
+require_relative "../lib/pieces/pawn"
+require_relative "../lib/board"
+require_relative "../lib/moves/abstract_move"
+
+
+describe Pawn do
+
+  subject(:pawn) { Pawn.new("white", [1, 2], Board.new) }
+
+  describe "#get_legal_moves" do
+
+    context "when called" do 
+
+      it "should return an array of AbstractMove objects" do
+        moves = pawn.get_legal_moves
+        expect(moves).to all(be_an(AbstractMove))
+      end
+    end
+
+
+
+  end
+
+  describe "#direction" do
+
+    context "when the piece is white" do
+
+      it "should return -1" do
+        expect(pawn.direction).to eq(-1)
+      end
+    end
+
+    context "when the piece is black" do
+      subject(:black_pawn) { Pawn.new("black", [1, 5], Board.new) }
+      it "should return 1 " do
+        expect(black_pawn.direction).to eq(1)
+      end
+    end
+  end
+
+
+end
